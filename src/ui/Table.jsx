3478d427960df66cee8632d5bd220db0e70f63ef
adarshperi <adarshperi@gmail.com>
2024-01-22 18:03:11 +0530

    adding compound components

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+/* eslint-disable react/prop-types */
+import styled from 'styled-components';
+import { createContext, useContext } from 'react';
 
 const StyledTable = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -57,3 +59,43 @@ const Empty = styled.p`
   text-align: center;
   margin: 2.4rem;
 `;
+
+const TableContext = createContext();
+
+const Table = ({ columns, children }) => {
+  return (
+    <TableContext.Provider value={{ columns }}>
+      <StyledTable role='table'>{children}</StyledTable>
+    </TableContext.Provider>
+  );
+};
+
+const Header = ({ children }) => {
+  const { columns } = useContext(TableContext);
+  return (
+    <StyledHeader role='row' columns={columns} as='header'>
+      {children}
+    </StyledHeader>
+  );
+};
+
+const Row = ({ children }) => {
+  const { columns } = useContext(TableContext);
+  return (
+    <StyledRow role='row' columns={columns}>
+      {children}
+    </StyledRow>
+  );
+};
+
+const Body = ({ data, render }) => {
+  if(!data?.length) return <Empty>No data to show at the moment.</Empty>
+  return <StyledBody>{data.map(render)}</StyledBody>;
+};
+
+Table.Header = Header;
+Table.Row = Row;
+Table.Body = Body;
+Table.Footer = Footer;
+
+export default Table;
