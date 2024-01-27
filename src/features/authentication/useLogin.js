856b8efe8324cee18cd3c87221716b8a2a248f33
adarshperi <adarshperi@gmail.com>
2024-01-27 12:35:19 +0530

    Setting user data

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,16 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../services/apiAuth';
 import { useNavigate } from 'react-router';
 import toast from 'react-hot-toast';
 
 export const useLogin = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  
   const { isPending: isLoading, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (data) => {
+      queryClient.setQueryData(['user'], data.user);
       navigate('/dashboard', { replace: true });
     },
     onError: (error) => {
